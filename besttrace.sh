acd84fcde738a33bf7cc8d201e45395da2870573
tsukiyoru <42717781+yylhz@users.noreply.github.com>
2020-02-26 19:37:48 +0800

    Create besttrace.sh

---
+++ besttrace.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# apt -y install unzip
+
+# install besttrace
+if [ ! -f "besttrace" ]; then
+    wget https://github.com/wn789/VPS-/raw/master/besttrace
+    # unzip besttrace4linux.zip
+    chmod +x besttrace
+fi
+
+## start to use besttrace
+
+next() {
+    printf "%-70s\n" "-" | sed 's/\s/-/g'
+}
+
+clear
+next
+
+ip_list=(112.5.37.138 119.188.248.87 42.7.26.38 111.6.79.107 223.166.92.235 222.186.136.65 )
+ip_addr=(泉州移动 山东联通 辽宁联通 河南移动 上海联通 镇江电信)
+# ip_len=${#ip_list[@]}
+
+for i in {0..15}
+do
+	echo ${ip_addr[$i]}
+	./besttrace -q 1 ${ip_list[$i]}
+	next
+done
